babb547f9d648c3593ab008e52ebb49aff6603e1
avlioluap <spooka8@gmail.com>
2017-01-13 17:58:19 +0000

    commit 3

--- fidelidadeVeiculos.py
+++ fidelidadeVeiculos.py
@@ -19,6 +19,19 @@ class FidelidadeVeiculos:
             print
             "Timed out waiting for page to load"
 
+        """
+        timeout = 2
+        try:
+            element_present = EC.presence_of_element_located((By.LINK_TEXT, 'AU-TO-IB MultiPlanos'))
+            WebDriverWait(self.driver, timeout).until(element_present)
+
+            self.driver.find_element_by_link_text('AU-TO-IB MultiPlanos').click()
+            self.iniciar()
+        except TimeoutException:
+            print
+            "Timed out waiting for page to load"
+        """
+
     def login(self):
         # dados de login TODO: ir buscar a base dados os valores
         username = 'XMD2168'
@@ -35,17 +48,38 @@ class FidelidadeVeiculos:
 
 
     def iniciar(self):
-        print("estou aqui")
-        """
+        # clicar em empresa ou particular TODO: obter tipo da BD
+        tipo = "particular"
+
+        if tipo == "particular":
+            self.driver.find_element_by_id('CAN_Common_Layout_wt18_block_wtbody_SimuladorAUTMP_ComUI_wtMxClientType_block_P').click()
+
+        if tipo == "empresa":
+            self.driver.find_element_by_id('CAN_Common_Layout_wt18_block_wtbody_SimuladorAUTMP_ComUI_wtMxClientType_block_C').click()
+
+        self.quantosVeiculos()
+
+    def quantosVeiculos(self):
+        # limite é de 10 TODO numVeiculos BD
+        numVeiculos = 7
         timeout = 2
         try:
-            element_present = EC.presence_of_element_located((By.LINK_TEXT, 'AU-TO-IB MultiPlanos'))
+            element_present = EC.presence_of_element_located((By.ID, 'CAN_Common_Layout_wt19_block_wtbody_wtContent'))
             WebDriverWait(self.driver, timeout).until(element_present)
 
-            self.driver.find_element_by_link_text('AU-TO-IB MultiPlanos').click()
-            self.iniciar()
+            if numVeiculos < 6:
+                self.driver.find_element_by_id('CAN_Common_Layout_wt19_block_wtbody_SimuladorAUTMP_ComUI_wtmxCtrVeicCount_block_' + str(numVeiculos)).click()
+            else:
+                for x in range(5, numVeiculos):
+                    self.driver.find_element_by_class_name('fa-caret-up').click()
+
+                self.driver.find_element_by_id('CAN_Common_Layout_wt19_block_wtbody_wtbtnContinueVehicleCount')
+
+                self.veiculosNaApolice()
+
         except TimeoutException:
             print
             "Timed out waiting for page to load"
-        """
 
+    def veiculosNaApolice(self):
+        print("sopas")
